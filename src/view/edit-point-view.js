a56dc5db058d513fccaea42be92a0d1d54155245
Yana Rozova <rozova.ys@gmail.com>
2024-02-26 18:29:45 +0300

    Add data to points

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,12 +1,14 @@
 import { createElement } from '../render.js';
 
 const createDestinationBlock = (destination = {}) => {
-  const { pictures = [] } = destination;
+  const { pictures = [], description='' } = destination;
 
   return (`
   <section class="event__section  event__section--destination">
   <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-  <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+  <p class="event__destination-description">
+    ${description}
+  </p>
 
   <div class="event__photos-container">
     <div class="event__photos-tape">
