23d0bbb83521628a0086956169fd7e742246de9e
Yana Rozova <rozova.ys@gmail.com>
2024-05-22 17:45:13 +0300

    Update content for empty list

--- src/mock/const.js
+++ src/mock/const.js
@@ -28,8 +28,8 @@ export const FilterType = {
 
 export const NoPointMessage = {
   [FilterType.EVERYTHING]: 'Click New Event to create your first point',
-  [FilterType.FUTURE]: 'There are no past events now',
-  [FilterType.PAST]: 'There are no future events now',
+  [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PAST]: 'There are no past events now',
 };
 
 export const SortType = {
