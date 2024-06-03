2042d9b43c03d8e288a8c8f2e788b194d4c8e91f
ChipRose <yana.rozova@mail.ru>
2024-06-03 15:23:13 +0000

    Deploying to gh-pages from @ ChipRose/grand-trip@cff35d3edb8b54eaaa92e73753d06fd023e38da8 🚀

--- src/mock/filter.js
+++
@@ -1,6 +0,0 @@
-import { filter } from '../util/filter-util';
-
-export const generateFilter = (points) => Object.entries(filter).map(([filterName, filterPoints]) => ({
-  name: filterName,
-  count: filterPoints(points)?.length
-}))
